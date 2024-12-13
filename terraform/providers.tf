d6a038b9fef5b990904e423ccabf1d070c43d675
tereza-arta <157483329+tereza-arta@users.noreply.github.com>
2024-12-13 18:11:17 +0400

    Create providers.tf

---
+++ terraform/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "****"
+  client_id       = "****"
+  tenant_id       = "****"
+
+  features {}
+}
